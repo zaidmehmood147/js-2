d1e62f6f4e52c67673c0c0f25c517fe57d3e35c4
unknown <kzaid2918@gmail.com>
2025-12-25 21:02:13 +0500

    Revision 25/12

--- 24-12-2025/main.js
+++ 24-12-2025/main.js
@@ -1,5 +1,16 @@
 // var uname = prompt("enter your name ");
 
+// // var arr = ["Saad", "Bilal", "Asad"];
+// var unameInLower = uname.toLowerCase();
+// var unameInUppercase = uname.toUpperCase();
+// for (var i = 0; i < uname.length; i++) {
+//   if (unameInUppercase == uname[i]) {
+//     console.log("milgaya", unameInUppercase);
+//     break;
+//   } else {
+//     console.log("sorry nhi mila");
+//   }
+
 // var arr = ["Saad", "Bilal", "Asad"];
 // //  var unameInLower = uname.toLowerCase()
 // //  var unameInUppercase = uname.toUpperCase()
@@ -14,9 +25,56 @@
 // //     console.log("uname=>",unameInUppercase)
 // // }
 
-// var firstChar = uname.slice(0, 1);
-// var firstChar = firstChar.toUpperCase();
-// // console.log("uppercase", firstChar);
+
+  // console.log(arr)
+
+  // console.log("uname=>",unameInUppercase)
+// }
+// var uname = prompt("Enter your name");
+// var arr = ["Zaid","Bial","Kashan"];
+// var upperCase = uname.toUpperCase();
+// var lowerCase = uname.toLowerCase();
+// for (i= 0; i < arr.length; i++) {
+//     if (upperCase==arr) {
+//         console.log("correct", upperCase)
+//     } else {
+//         console.log("Incorrect")
+//     }
+
+// var uname = prompt("Enter your name");
+// var arr = ["Zaid","Bial","Kashan"];
+
+// var userName = uname.toLowerCase();
+// var found = false;
+
+// for (var i = 0; i < arr.length; i++) {
+//     if (userName === arr[i].toLowerCase()) {
+//         found = true;
+//         break;
+//     }
+// }
+
+// if (found) {
+//     console.log("Correct");
+// } else {
+//     console.log("Incorrect");
+// }
+
+// }
+// var arr = ["bial","zaid",40,false]
+// arr[200]= "kashan"
+// console.log(arr)
+
+// var arr = ["bial","zaid",40,false]
+// arr[200]= "kashan"
+// arr.push("bionzoo")
+// //  arr.pop()
+
+// var uname = ["zaid","class","index","length"]
+// // var firstChar = uname.slice(0, 2);
+// var firstChar = uname.splice(0,3)
+// // var firstChar = firstChar.toUpperCase();
+// console.log("uppercase", firstChar);
 
 // //  steps to solve a specific problem
 // var remainingChar = uname.slice(1);
@@ -36,14 +94,12 @@
 
 // // var text = "this is World War II in asia 1945"
 //           //        World War II
-        
 
-
-// var number = Math.round(0.1)
-// var number = Math.floor(1.8)
+// var number = Math.round(0.6)
+// // var number = Math.floor(1.8)
 // console.log("number",number)
 
 // var number = prompt("enter your age")
 
-// var age = parseFloat(number) + 10
-// console.log(age)v
+// var age = parseFloat(number) // + 10
+// console.log(age)
