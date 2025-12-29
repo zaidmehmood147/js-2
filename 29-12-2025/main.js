d7531fb49d42c696f9bf60127123127c5f6056fa
unknown <kzaid2918@gmail.com>
2025-12-29 14:41:57 +0500

    Events

---
+++ 29-12-2025/main.js
@@ -0,0 +1,9 @@
+// EVENTS
+// On website when you click or do smth it response 
+// it gives output
+// if we have () it can be function or method
+
+
+// function test(msg) {
+//     alert(message);
+// }
